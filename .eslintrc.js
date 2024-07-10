80555e136daec35438b3469bfa1d72e3ce6d5fa4
백승주 <137677440+bearkuang@users.noreply.github.com>
2024-07-10 20:21:47 +0900

    [Update] ESLint 설정, gitHub Actions 워크플로우 수정

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['react-app'],
+    rules: {
+      '@typescript-eslint/no-redeclare': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'jsx-a11y/img-redundant-alt': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  };
